3929e1e7be4578e3da26fdae4802c8630b23904d
Roma <romamakarcikov@gmail.com>
2023-03-08 22:43:02 +0300

    Add fix of homrwork 6

--- src/my/task/hw6/Task3.java
+++ src/my/task/hw6/Task3.java
@@ -5,28 +5,34 @@ import java.util.Arrays;
 public class Task3 {
 
     public static void main(String[] args) {
+        System.out.println("Text:");
         String text = "Hello,3333333333      bye;;;;; me a r sfsdfs 4444444444  g ";
         System.out.println(text);
-        System.out.println("Result");
+        System.out.println("Result:");
         replaseMinAndMaxWord(text);
     }
 
     private static void replaseMinAndMaxWord(String text) {
         String regex = "\\W+";
         String[] words = text.split(regex);
-        String maxword = "";
-        String minsword = words[0];
-        String perem;
-        for (String word : words) {
-            if(word.length()>maxword.length()) {
-                maxword = word;
-            } else  if(word.length()<=minsword.length()) {
-                minsword = word;
+        int maxLength = words[0].length();
+        int minLength = words[0].length();
+        String maxWord = words[0];
+        String minWord = words[0];
+        int maxIndex = 0, minIndex = 0;
+        for (int i = 0; i <= words.length - 1; i++) {
+            if (words[i].length() > maxLength) {
+                maxLength = words[i].length();
+                maxWord = words[i];
+                maxIndex = i;
+            } else if (words[i].length() <= minLength) {
+                minLength = words[i].length();
+                minIndex = i;
+                minWord = words[i];
             }
         }
-        perem = maxword;
-        maxword = minsword;
-        minsword = perem;
-        System.out.println(maxword+" "+ minsword);
+        words[maxIndex] = minWord;
+        words[minIndex] = maxWord;
+        System.out.println(Arrays.toString(words));
     }
 }
--- src/my/task/hw6/Task5.java
+++ src/my/task/hw6/Task5.java
@@ -1,12 +1,11 @@
 package my.task.hw6;
-import java.util.Arrays;
 
 public class Task5 {
     public static void main(String[] args) {
 
         String text = "Object-oriented programming is a programming language model organized around objects rather than" +
                 " \"actions\" and data rather than logic. Object-oriented programming blabla. Object-oriented programming bla.";
-
+        System.out.println("Result:");
         replacingOop(text);
     }
 
@@ -14,12 +13,16 @@ public class Task5 {
         String regex = "Object-oriented programming";
 
         String[] words = text.split(regex);
-        for(int i = 1; i< words.length;i++) {
+        for (int i = 1; i < words.length; i++) {
             words[i] = regex + words[i];
-            if(i % 2 ==0) {
-                words[i] = words[i].replaceAll(regex,"OOP");
+            if (i % 2 == 0) {
+                words[i] = words[i].replaceAll(regex, "OOP");
             }
         }
-       System.out.println(Arrays.toString(words));
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String word : words) {
+            stringBuilder.append(word);
+        }
+        System.out.println(stringBuilder);
     }
 }
