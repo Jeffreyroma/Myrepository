3f5c513c1e1bb25125f34803483c532cc0cbfb9d
Roma <romamakarcikov@gmail.com>
2023-04-11 18:43:48 +0300

    commit my changes for switch branches

--- src/my/task/hw8/Task3/Cap.java
+++
@@ -1,9 +0,0 @@
-package my.task.hw8;
-
-public class Cap extends Products  {
-
-
-    public Cap(String size, String color, String name) {
-        super(size, color, name);
-    }
-}
--- src/my/task/hw8/Task3/Shirt.java
+++
@@ -1,8 +0,0 @@
-package my.task.hw8;
-
-public class Shirt extends Products {
-
-    public Shirt(String size, String color, String name) {
-        super(size, color, name);
-    }
-}
