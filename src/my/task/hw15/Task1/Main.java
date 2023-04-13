a1b6231bf37dc9e0f85c4e8fa928aaf864ee6e4a
Roma <romamakarcikov@gmail.com>
2023-04-13 18:20:06 +0300

    Add files with homework 15

---
+++ src/my/task/hw15/Task1/Main.java
@@ -0,0 +1,27 @@
+package my.task.hw15.Task1;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Collection<String> col = new ArrayList<>();
+        col.add("login");
+        col.add("Hell");
+        col.add("Hello, my friend");
+        boolean flag = col.stream().anyMatch(word -> word.equalsIgnoreCase("login"));
+        System.out.println("The word 'login' is contained in the collection? " + flag);
+        System.out.println("\nMax length:");
+        System.out.println(col.stream().max(Comparator.comparing(String::length)).get());
+        System.out.println("\nMin length:");
+        System.out.println(col.stream().min(Comparator.comparing(String::length)).get());
+        System.out.println("\nWords without punctuation marks and spaces:");
+        List<String> onlyWords = col.stream().filter(word->word.matches("[a-zA-Z1-9]*")).toList();
+        System.out.println(onlyWords);
+        System.out.println("\nAll words");
+        List<String> allWords = col.stream().flatMap(word -> Arrays.stream(word.split(" ")))
+                .map(word->word.replaceAll("[^a-zA-Z]", "")).collect(Collectors.toList());
+        System.out.println(allWords);
+    }
+}
---
+++ src/my/task/hw15/Task2/Main.java
@@ -0,0 +1,23 @@
+package my.task.hw15.Task2;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Comparator;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Collection<Integer> col = new ArrayList<>();
+        col.add(12);
+        col.add(23);
+        col.add(1);
+        col.add(0);
+        System.out.println("Max number is: " + col.stream().max(Comparator.naturalOrder()).get());
+        System.out.println("Min number is: " + col.stream().min(Comparator.naturalOrder()).get());
+        System.out.println("Arithmetic mean of numbers is : " + col.stream().mapToDouble(Integer::doubleValue).average().getAsDouble());
+        System.out.println("The product of all numbers is : " + col.stream().reduce((num1, num2) -> num1 * num2).get());
+        System.out.println("The sum of all the numbers is : " + col.stream().reduce(Integer::sum).get());
+        System.out.println("The sum of all digits is : " + col.stream().map(digit->Integer.toString(digit)).flatMapToInt(CharSequence::chars)
+                .map(Character::getNumericValue).sum());
+    }
+}
